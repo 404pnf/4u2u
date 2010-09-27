b3bcf5005262b7834145d404dc17dfdad2be4138
404 pnf <baixiaoyu@gmail.com>
2010-09-27 15:18:49 +0800

    更新了问答模块，新增加了以下功能：
    1、当有人回答时，邮件通知提问者
    2、当提问者选择最佳答案后，邮件通知最佳答案回答者
    3、提问要到30天的时候，提前一周发邮件通知提问者
    4、提问超过30天但仍然没有选择最佳答案，问题状态变为过期，最佳答案id设置-1
    
    同时更新了一点问答模板

---
+++ sites/answer.2u4u.com.cn/themes/answer/node-best_answer.tpl.php
@@ -0,0 +1,30 @@
+<div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
+  <div class="node-inner">
+	
+	<?php if (!empty($title)): ?>
+		<div class="title_bg"><h1 class="title"><?php print $title; ?></h1></div>
+	<?php endif; ?>
+	
+    <?php print $picture; ?>
+
+    <?php if ($submitted): ?>
+      <span class="submitted clear"><?php print $submitted; ?></span>
+    <?php endif; ?>
+	<?php if ($terms): ?>
+       <div class="taxonomy clear"><?php print $terms; ?></div>
+    <?php endif;?>
+
+    <div class="content clear">
+      <?php print $content; ?>
+    </div>
+	
+	<?php if ($links): ?> 
+		  <div class="links clear"> <?php print $links; ?></div>
+	<?php endif; ?>
+		
+  </div> <!-- /node-inner -->
+</div> <!-- /node-->
+
+<div class="node_bestanswer">
+	<?php print drupal_render($bestanswer); ?>
+</div>

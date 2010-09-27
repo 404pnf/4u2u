a27b0a870e757ab703e730b074f34543dba8eb33
404 pnf <baixiaoyu@gmail.com>
2010-09-27 16:31:11 +0800

    shit happens

--- sites/2u4u.com.cn/modules/custom/ucenter/uc_client/data/cache/apps.php
+++ sites/2u4u.com.cn/modules/custom/ucenter/uc_client/data/cache/apps.php
@@ -1,55 +1,52 @@
 <?php
 $_CACHE['apps'] = array (
-  1 => 
-  array (
+1 =>
+array (
     'appid' => '1',
     'type' => 'UCHOME',
-    'name' => '悠游外语家园',
-    'url' => 'http://u.2u4u.com.cn/home',
+    'name' => '个人家园',
+    'url' => 'http://u.2n4n.com.cn/home',
     'ip' => '',
     'viewprourl' => '',
     'apifilename' => 'uc.php',
     'charset' => 'utf-8',
     'synlogin' => '1',
-    'extra' => 
-    array (
-      'apppath' => ' ',
-    ),
+    'extra' => '',
     'recvnote' => '1',
-  ),
-  2 => 
-  array (
+),
+2 =>
+array (
     'appid' => '2',
     'type' => 'DISCUZ',
-    'name' => '悠游外语论坛',
-    'url' => 'http://u.2u4u.com.cn/bbs',
+    'name' => 'Discuz!',
+    'url' => 'http://u.2n4n.com.cn/bbs',
     'ip' => '',
     'viewprourl' => '',
     'apifilename' => 'uc.php',
     'charset' => 'utf-8',
     'synlogin' => '1',
     'extra' => 
-    array (
-      'apppath' => ' ',
-    ),
+array (
+      'apppath' => '',
+),
     'recvnote' => '1',
-  ),
-  3 => 
-  array (
+),
+3 =>
+array (
     'appid' => '3',
     'type' => 'OTHER',
-    'name' => '悠游外语学习平台',
-    'url' => 'http://2u4u.com.cn/sites/2u4u.com.cn/modules/custom/ucenter',
+    'name' => 'drupal',
+    'url' => 'http://2n4n.com.cn/',
     'ip' => '',
     'viewprourl' => '',
     'apifilename' => 'uc.php',
     'charset' => '',
     'synlogin' => '1',
     'extra' => 
-    array (
-      'apppath' => ' ',
-    ),
+array (
+      'apppath' => '',
+),
     'recvnote' => '1',
-  ),
-  'UC_API' => 'http://u.2u4u.com.cn/ucenter',
+),
+  'UC_API' => 'http://u.2n4n.com.cn/ucenter',
 );

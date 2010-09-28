894f5e55ab78f20c96b3df92d9c1c36e7d36b93b
404 pnf <baixiaoyu@gmail.com>
2010-09-28 14:44:37 +0800

    update css and index of games

--- games/index.php
+++ games/index.php
@@ -5,12 +5,15 @@
 <title>2U4U单词大赛</title>
 <link rel="stylesheet" type="text/css" media="screen" href="styles/styles.css" />
 <style type="text/css">
-body{background:url(images/bg.jpg) no-repeat left top;}
+body {
+background:url("images/bg.jpg") no-repeat scroll center top #A81F26;
+}
 </style>
 <script language="javascript">AC_FL_RunContent = 0;</script>
 <script src="AC_RunActiveContent.js" language="javascript"></script>
 </head>
 <body bgcolor="#A81F26">
+<div id="main">
 <div id="logo"><img src="images/logo.png"/></div>
 <div id="user_info"><?php echo $user_name."(".$user_id.")，欢迎您！";?></div>
 <div class="nav">
@@ -20,6 +23,7 @@ body{background:url(images/bg.jpg) no-repeat left top;}
          <li><a href="sj/index.php">单词射击游戏</a></li>
      </ul>
      <div class="footer"><span>外语教学与研究出版社版权所有 Copyright © 2008 2U4U.com.cn All Rights Reserved</span></div>
+</div>
 </div>
 </body>
 </html>

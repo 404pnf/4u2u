f77fb8f211a7db19eba56e1f119716c1ae7eb2be
404 pnf <baixiaoyu@gmail.com>
2011-05-19 08:19:44 +0800

    增加21st的模板和图片

--- sites/2u4u.com.cn/export_xml/ucenter_xml.php
+++ sites/2u4u.com.cn/export_xml/ucenter_xml.php
@@ -1,4 +1,5 @@
 <?php
+//report php error
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE); 
@@ -8,10 +9,16 @@ require 'connect.php';
 global $discuz_url;
 $discuz_url = "http://u.2u4u.com.cn/";
 $time = strtotime("today");
-$sql = "SELECT blogid,uid,username,subject 
+$sql = "SELECT ub.blogid,ub.uid,ub.username,ub.subject,uf.message
+               FROM `uchome_blog` ub
+	       LEFT JOIN `uchome_blogfield` uf
+               ON ub.blogid=uf.blogid
+	       WHERE dateline>$time-86400*7 GROUP BY uid ORDER BY hot DESC,viewnum DESC
+	       LIMIT 4";
+/*$sql = "SELECT blogid,uid,username,subject 
 	       FROM `uchome_blog` 
 	       WHERE dateline>$time-86400*7 GROUP BY uid ORDER BY hot DESC,viewnum DESC 
-	       LIMIT 4";		    
+	       LIMIT 4";*/		    
 $rs =  mysql_query($sql);
 $fic = fopen("export.xml","w");    
 /*fwrite($fic, '<?xml version="1.0"encoding="ISO-8859-1"?>');
@@ -22,10 +29,12 @@ DEFINE('UC_API',"http://u.2u4u.com.cn/ucenter");
     while($row = mysql_fetch_object($rs)){
 	fwrite($fic, "<node>");      
     	$photo = '';
-    	$photo = ckavatar($row->uid) ? avatar($row->uid, 'small',true) : UC_API.'/images/noavatar_small.gif';
+	$photo = ckavatar($row->uid) ? avatar($row->uid, 'small',true) : UC_API.'/images/noavatar_small.gif';
+	$message = substr(trim(strip_html_tags($row->message)),0,50);
 	fwrite($fic,'<avatar>'.$photo.'</avatar>');
 	fwrite($fic,'<id>'.$row->username.'</id>');
 	fwrite($fic,'<title>'.$row->subject.'</title>');
+	fwrite($fic,'<content>'.$message.'</content>');
 	fwrite($fic,'<url>'.$discuz_url."home/space.php?uid=".$row->uid.'</url>');
       	fwrite($fic,'</node>');
     }
@@ -63,4 +72,48 @@ function ckavatar($uid) {
 		return file_exists($file)?1:0;
 	
 }
+
+/**
+ * Remove HTML tags, including invisible text such as style and
+ * script code, and embedded objects.  Add line breaks around
+ * block-level tags to prevent word joining after tag removal.
+ */
+function strip_html_tags($text)
+{
+    $text = preg_replace( 
+     array ("'<script[^>]*?>.*?</script>'si",  // &#21435;&#25481; javascript
+                "'<[\/\!]*?[^<>]*?>'si",           // &#21435;&#25481; HTML &#26631;&#35760;
+                "'([\r\n])[\s]+'",                 // &#21435;&#25481;&#31354;&#30333;&#23383;&#31526;
+                "'&(quot|#34);'i",                 // &#26367;&#25442; HTML &#23454;&#20307;
+                "'&(amp|#38);'i",
+                "'&(lt|#60);'i",
+                "'&(gt|#62);'i",
+                "'&(nbsp|#160);'i",
+                "'&(iexcl|#161);'i",
+                "'&(cent|#162);'i",
+                "'&(pound|#163);'i",
+                "'&(copy|#169);'i",
+                "'&#(\d+);'e",// &#20316;&#20026; PHP &#20195;&#30721;&#36816;&#34892;
+            ),
+
+     array ("",
+                 "",
+                 "\\1",
+                 "\"",
+                 "&",
+                 "<",
+                 ">",
+                 " ",
+                 chr(161),
+                 chr(162),
+                 chr(163),
+                 chr(169),
+                "chr(\\1)"
+           ),
+        $text );
+    return $text;
+}
+
+
+
 ?>

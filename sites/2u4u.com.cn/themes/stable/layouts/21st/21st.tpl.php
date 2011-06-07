f9a21dce1e333090b69a060af3956102b54febd2
404 pnf <baixiaoyu@gmail.com>
2011-06-07 13:38:02 +0800

    把google analytics模块放在 sites/all 所有站点都启用它

---
+++ sites/2u4u.com.cn/themes/stable/layouts/21st/21st.tpl.php
@@ -0,0 +1,41 @@
+
+<div class="zhuanqu_21st">
+   <div class="zq_title_21st">                                                                                             
+     <?php print $content['zq_title_21st']; ?>                                                                        
+   </div>
+   <div class="zq_content_21st">
+       <div class="box_21st">
+        
+          <div class="column_21st_l1">
+            <?php print $content['column_21st_l1']; ?>  
+          </div>
+          <div class="column_21st_l2">
+            <?php print $content['column_21st_l2']; ?>
+          </div>
+          <div class="column_21st_r1">
+            <?php print $content['column_21st_r1']; ?>  
+          </div>
+          <div class="column_21st_r2">
+            <?php print $content['column_21st_r2']; ?>  
+          </div>
+       </div>
+       <div class="dl_21st">
+            <?php print $content['dl_21st']; ?> 
+       </div>
+   </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

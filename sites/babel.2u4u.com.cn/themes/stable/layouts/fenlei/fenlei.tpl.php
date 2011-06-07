2eac250ea91d3b8e3e384f980da8fba4e237261a
404 pnf <baixiaoyu@gmail.com>
2011-06-07 13:37:04 +0800

    babel站点主题修改了

---
+++ sites/babel.2u4u.com.cn/themes/stable/layouts/fenlei/fenlei.tpl.php
@@ -0,0 +1,29 @@
+<div class="fenlei1">
+      <?php print $content['fenlei1']; ?>
+</div>
+
+<div class="fenlei2">
+      <?php print $content['fenlei2']; ?>
+</div>
+
+<div class="fenlei3">
+      <?php print $content['fenlei3']; ?>
+</div>
+
+<div class="fenlei4">
+      <?php print $content['fenlei4']; ?>
+</div>
+
+<div class="fenlei5">
+      <?php print $content['fenlei5']; ?>
+</div>
+
+<div class="fenlei6">
+      <?php print $content['fenlei6']; ?>
+</div>
+
+<div class="fenlei7">
+      <?php print $content['fenlei7']; ?>
+</div>
+
+
---
+++ sites/babel.2u4u.com.cn/themes/stable/layouts/frame/frame.tpl.php
@@ -0,0 +1,25 @@
+<div id="classic_left" class="classic_left">
+	<div class="section" id="classic_flash">
+          <?php print $content['classic_flash']; ?>  
+        </div>
+           
+	<div class="section" id="classic_show_list">
+          <?php print $content['classic_show_list']; ?> 
+        </div>
+</div>
+
+<div id="classic_right" class="classic_right">
+       <div class="section" id="classic_note">
+          <?php print $content['classic_note'];?> 
+       </div>
+       
+       <div class="section" id="classic_elearning">
+          <?php print $content['classic_elearning']; ?> 
+       </div>
+
+       <div id="classic_ad">
+          <?php print $content['classic_ad']; ?>
+       </div>
+
+</div>
+
